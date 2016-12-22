444bbd401d59fb3d45cc43412479782b0b0e1be3
Andy O'Neill <aoneill01@gmail.com>
2016-12-21 19:44:45 -0500

    Adding knockout.

---
+++ PlanningPoker/Scripts/room.js
@@ -0,0 +1,33 @@
+﻿$(function () {
+    for (var i = 0; i < 5; i++) {
+        setTimeout(foo(i + 1), i * 200);
+    }
+
+    var picked = false;
+    $('#pick').click(function () {
+        if (!picked) {
+            $('.hand').addClass('pick');
+            $(this).html("Flip!");
+            picked = true;
+        } else {
+            $('.hand').addClass('flip');
+            $(this).css('display', 'none');
+        }
+    });
+});
+
+function foo(nth) {
+    return function () {
+        think(nth);
+        setInterval(think(nth), 2000);
+    }
+}
+
+function think(nth) {
+    return function () {
+        $('.hand .card:nth-child(' + nth + ')').addClass('think');
+        setTimeout(function () {
+            $('.hand .card:nth-child(' + nth + ')').removeClass('think');
+        }, 500);
+    };
+}
